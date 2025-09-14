fb693f386c5d39e4525022d0e57e6763c6eb7bbb
Kevin Hernandez <kevinhernandezcrespo97@gmail.com>
2025-09-14 12:17:39 -0500

    feat: agregar soporte para PostHog y validar configuración en el cliente de instrumentación

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,15 @@
+import posthog from "posthog-js";
+
+if (
+  !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
+  !process.env.NEXT_PUBLIC_POSTHOG_HOST
+) {
+  throw new Error(
+    "Missing NEXT_PUBLIC_POSTHOG_KEY or NEXT_PUBLIC_POSTHOG_HOST"
+  );
+}
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24",
+});
